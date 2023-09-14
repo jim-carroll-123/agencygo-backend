843cb0b0d7b2b4e5f1ec978196233bd4243c7b50
developeranku <ankur4736@gmail.com>
2023-09-14 10:20:04 +0530

    implemented captcha bypass

--- src/scraper/main.ts
+++ src/scraper/main.ts
@@ -1,62 +1,95 @@
-import path from 'path';
 import puppeteer from 'puppeteer-extra';
-import agents from 'user-agents';
-import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import SessionPlugin, { StorageProviderName } from 'puppeteer-extra-plugin-session';
-import fs from 'fs';
-import { Document } from 'mongoose';
+import { Page, executablePath } from 'puppeteer';
+import path from 'path';
 
-puppeteer.use(StealthPlugin());
-puppeteer.use(
-  RecaptchaPlugin({
-    provider: {
-      id: '2captcha',
-      token: '054855c660c6e582c36a3e209fbeace3',
-    },
-    visualFeedback: true, // colorize reCAPTCHAs (violet = detected, green = solved)
-  }),
-);
-puppeteer.use(SessionPlugin());
-
-(async () => {
-  const win = await puppeteer.launch({
-    headless: false,
-    userDataDir: path.join(__dirname, './temp'),
-    defaultViewport: null,
-    args: ['--start-maximized', '--disable-extensions', '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'],
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+const login = async (page: Page) => {
+  // await new Promise((r) => setTimeout(r, 15000));
+  const url = 'https://onlyfans.com/';
+  await page.goto(url, {
+    waitUntil: ['load', 'domcontentloaded'],
   });
-  const page = await win.newPage();
-  await page.setViewport({ width: 1366, height: 768 });
-  await page.setUserAgent(agents.random().toString());
+  await page.waitForNavigation();
 
-  const login = async () => {
-    await page.goto('https://onlyfans.com', {
-      waitUntil: ['load', 'domcontentloaded'],
-    });
-    await page.waitForNavigation();
-    await page.waitForTimeout(5000);
-    await page.type('input[at-attr="input"][name="email"]', 'ankur4736@gmail.com');
-    await page.type('input[at-attr="input"][name="password"]', 'Test@123');
+  await new Promise(r => setTimeout(r, 5000));
+
+  const inputLogin = async (email: string, password: string) => {
+    await page.type('input[at-attr="input"][name="email"]', email);
+    await page.type('input[at-attr="input"][name="password"]', password);
+  };
+
+  const clickLogin = async () => {
     await page.click('button[at-attr="submit"][type="submit"]');
-    await page.waitForTimeout(5000);
-    /* Check if captcha is there */
-    const captcha = await page.$$('div[data-v-0a03e54f][data-v-06a525d1].captcha_wrapper.g-input__wrapper.m-captcha.m-visible');
+  };
+
+  const checkingForCaptcha = async () => {
+    console.log('checking for captcha');
+    await new Promise(r => setTimeout(r, 3000));
+    const captcha = await page.$('.captcha-solver');
 
     if (captcha) {
-      const captchaSolver = await page.solveRecaptchas();
-      console.log(captchaSolver);
-      /* const frameHandle = await page.$('iframe[title="reCAPTCHA"]');
-      const frame = await frameHandle.contentFrame();
-      await frame.click('#rc-anchor-container');
-      await page.evaluate(() => {
-        const button = document.querySelector('button[data-v-06a525d1][at-attr="submit"].g-btn.m-rounded.m-block.m-md.mb-0');
-        button.removeAttribute('disabled');
-      });
-      await page.click('button[at-attr="submit"][type="submit"]'); */
+      let valid = false;
+      console.log('on lopping');
+      while (!valid) {
+        await Promise.all([
+          new Promise(async resolver => {
+            const ready = await page.$('.captcha-solver[data-state="ready"]');
+            if (ready) {
+              console.log('captcha ready');
+              await page.click('.captcha-solver[data-state="ready"]');
+            }
+            resolver(1);
+          }),
+          new Promise(async resolver => {
+            const error = await page.$('.captcha-solver[data-state="error"]');
+            if (error) {
+              console.log('captcha error');
+              await page.click('.captcha-solver[data-state="error"]');
+            }
+            resolver(1);
+          }),
+          new Promise(async resolver => {
+            const done = await page.$('.captcha-solver[data-state="solved"]');
+            if (done) {
+              console.log('captcha done');
+              valid = true;
+              await new Promise(r => setTimeout(r, 5000));
+              await clickLogin();
+            }
+            resolver(1);
+          }),
+        ]);
+      }
     }
   };
 
-  await login();
-})();
+  await inputLogin('ankur4736@gmail.com', 'Test@123');
+  await checkingForCaptcha();
+  await clickLogin();
+  await new Promise(r => setTimeout(r, 5000));
+
+  // try check 3 times
+  await checkingForCaptcha();
+  await checkingForCaptcha();
+  await checkingForCaptcha();
+
+  // get cookies
+  const cookies = await page.cookies();
+  console.log(cookies);
+
+  // close
+  // await page.close();
+};
+
+const main = async () => {
+  const pathToExtension = path.join(__dirname, './extensions/2captcha-solver');
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
+  await login(page);
+  // browser.close();
+};
+
+main();
