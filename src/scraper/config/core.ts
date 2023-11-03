cc5863dc561984432709020155ea0eb3ff087ee4
developeranku <ankur4736@gmail.com>
2023-11-03 15:21:24 +0530

    enabled dynamic proxy settings

--- src/scraper/config/core.ts
+++ src/scraper/config/core.ts
@@ -3,6 +3,29 @@ import { executablePath } from 'puppeteer';
 import path from 'path';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import pluginProxy from 'puppeteer-extra-plugin-proxy';
+import fs from 'fs';
+
+// Define the path to the config.js file
+const modifyProxySettingsInExtension = proxyUrl => {
+  const configFilePath = path.join(__dirname, '../extensions/2captcha-solver/common/config.js');
+  fs.readFile(configFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading config file:', err);
+    } else {
+      // Find and replace the 'proxy' value using a regular expression
+      const updatedContent = data.replace(/proxy:\s*".*?"/, `proxy: "${proxyUrl}"`).replace(/useProxy:\s*false/, 'useProxy: true');
+
+      // Write the updated content back to the file
+      fs.writeFile(configFilePath, updatedContent, err => {
+        if (err) {
+          console.error('Error writing config file:', err);
+        } else {
+          console.log('Proxy variable updated successfully.');
+        }
+      });
+    }
+  });
+};
 
 export const getBrowserInstance = async (
   proxy: {
@@ -13,6 +36,8 @@ export const getBrowserInstance = async (
   },
   userDataDir?: string,
 ) => {
+  const proxyUrl = `${proxy.username}:${proxy.password}@${proxy.hostname}:${proxy.port}`;
+  modifyProxySettingsInExtension(proxyUrl);
   puppeteer.use(StealthPlugin());
   puppeteer.use(
     pluginProxy({
--- src/scraper/extensions/2captcha-solver/common/config.js
+++ src/scraper/extensions/2captcha-solver/common/config.js
@@ -38,9 +38,9 @@ var Config = {
     repeatOnErrorTimes: 5,
     repeatOnErrorDelay: 5,
     buttonPosition: "inner",
-    useProxy: false,
-    proxytype: "HTTP",
-    proxy: "",
+    useProxy: true,
+    proxytype: "HTTPS",
+    proxy: "ZODHCa:ZODHCa_country-us@geo.iproyal.com:12321",
     blackListDomain: "example.com\n2captcha.com/auth\nrucaptcha.com/auth",
     normalSources: [],
     autoSubmitRules: [
