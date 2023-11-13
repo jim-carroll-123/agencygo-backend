84fdb243e55e1186d9682009ddcb78c13e4c6030
kishan <kishan@sandzglobal.com>
2023-11-13 12:55:44 +0100

    integrated twilio

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,13 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_SECRET_KEY, TWILIO_API_KEY } from '@config';
-import twilio from 'twilio';
+import twilio, { Twilio } from 'twilio';
 
 import { Server as HttpServer } from 'http';
 import { Server as Socket } from 'socket.io';
+import { Service } from 'typedi';
+import * as axios from 'axios';
+
+import { TwilioModel } from '@/models/twilio.model';
+import { UserModel } from '@/models/users.model';
+import { email } from 'envalid';
 
 let io: Socket;
 
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+const twilioBaseUrl = 'https://conversations.twilio.com/v1';
 
 export const initializeWebSocket = (httpServer: HttpServer) => {
   io = new Socket(httpServer, {
@@ -36,7 +43,7 @@ export const getSocketIO = () => {
   }
   return io;
 };
-
+@Service()
 export class ChatController {
   public generateToken = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -93,9 +100,41 @@ export class ChatController {
   public createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userEmail = req.body.email;
-      const data = await client.conversations.v1.users.create({ identity: userEmail });
+      const friendlyname = req.body.name;
+      const data = await client.conversations.v1.users.create({ identity: userEmail, friendlyName: friendlyname });
+      await UserModel.updateOne({ email: userEmail }, { $set: { twilioUserId: data.sid } });
+      res.status(201).json({
+        message: 'User created successfully',
+      });
+    } catch (error) {
+      res.status(400).json({
+        error,
+      });
+    }
+  };
+
+  public fetchAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await client.conversations.v1.users.list({ limit: 20 });
+      const userSids = users.map(user => user.sid);
+
       res.status(200).json({
-        user: data,
+        users: userSids,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: 'Error fetching users',
+      });
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.params.userId; // Assuming the user ID is part of the URL parameters
+      await client.conversations.v1.users(userId).remove();
+      res.status(200).json({
+        message: 'User deleted successfully',
       });
     } catch (error) {
       console.log(error);
@@ -106,16 +145,16 @@ export class ChatController {
   };
   public createConversation = async (req: Request, res: Response) => {
     try {
-      // const user = await client.conversations.v1.users(req.body.email).fetch();
-      const { friendlyName, username } = req.body;
+      const { friendlyName, userEmail } = req.body;
       const conversation = await client.conversations.v1.conversations.create({
         friendlyName: friendlyName,
       });
-      // const token = this.createToken(username, conversation.chatServiceSid);
-
-      // req.header['username'] = username;
-      const participant = await client.conversations.v1.conversations(conversation.sid).participants.create({ identity: username });
-
+      const finduser = await UserModel.findOne({ email: userEmail });
+      if (!finduser.twilioUserId) {
+        const data = await client.conversations.v1.users.create({ identity: userEmail });
+        await UserModel.updateOne({ email: userEmail }, { $set: { twilioUserId: data.sid } });
+      }
+      const participant = await client.conversations.v1.conversations(conversation.sid).participants.create({ identity: userEmail });
       res.status(200).json({
         conversation,
         participant,
@@ -139,9 +178,6 @@ export class ChatController {
         const conversation = await client.conversations.v1.conversations.get(conversationSid).fetch();
 
         if (username && conversationSid) {
-          // req.header['token'] = this.createToken(username, conversation.chatServiceSid);
-          // req.header['username'] = username;
-
           const participant = await client.conversations.v1.conversations(conversationSid).participants.create({ identity: username });
 
           res.send({ conversation, participant });
@@ -173,7 +209,7 @@ export class ChatController {
     }
   };
 
-  public getConversations = async (req: Request, res: Response) => {
+  public getAllConversations = async (req: Request, res: Response) => {
     try {
       const limit = Number(req.query.limit) || 20;
       const arr = [];
@@ -188,6 +224,75 @@ export class ChatController {
       });
     } catch (error) {
       console.log(error);
+      res.status(400).json({
+        error,
+      });
+    }
+  };
+
+  public getConversations = async (req: Request, res: Response) => {
+    try {
+      const arr = [];
+      const userData = req.user;
+      const response = await axios.get(`${twilioBaseUrl}/Users/${userData.twilioUserId}/Conversations`, {
+        auth: {
+          username: TWILIO_ACCOUNT_SID,
+          password: TWILIO_AUTH_TOKEN,
+        },
+      });
+      const data = response.data.conversations;
+      data.forEach(c => {
+        arr.push({
+          friendlyName: c.friendly_name,
+          cID: c.conversation_sid,
+          url: c.url,
+          conversationState: c.conversation_state,
+          links: c.links,
+        });
+      });
+
+      res.status(200).json({
+        arr,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        error,
+      });
+    }
+  };
+
+  public getMessages = async (req: Request, res: Response) => {
+    try {
+      const arr = [];
+      const userData = req.user;
+      const cID = req.params.cID;
+      // https://conversations.twilio.com/v1/Conversations/CHe54af793d61d48d38080d070bba9f39b/Messages
+      const response = await axios.get(`${twilioBaseUrl}/Conversations/${cID}/Messages`, {
+        auth: {
+          username: TWILIO_ACCOUNT_SID,
+          password: TWILIO_AUTH_TOKEN,
+        },
+      });
+      const data = response.data.messages;
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  public getUserConversations = async (req: Request, res: Response) => {
+    try {
+      const userSid = req.params.userSid; // Extract user SID from request parameters
+
+      // Fetch conversations for the user
+      const conversations = await client.conversations.v1.users(`${userSid}`).userConversations.list({ limit: 20 });
+
+      res.status(200).json({
+        conversations,
+      });
+    } catch (error) {
+      console.error(error);
       res.status(400).json({
         error,
       });
--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -1,4 +1,5 @@
 import { ChatController } from '@/controllers/chat.controller';
+import { AuthMiddleware } from '@/middlewares/auth.middleware';
 import { Routes } from '@interfaces/routes.interface';
 import { Router } from 'express';
 
@@ -14,12 +15,17 @@ export class ChatRoute implements Routes {
   private initializeRoutes() {
     this.router.post(`${this.path}/chattoken`, this.chat.generateToken);
     this.router.post(`${this.path}/user`, this.chat.createUser);
+    this.router.get(`${this.path}/user`, this.chat.fetchAllUsers);
+    this.router.delete(`${this.path}/user/:userId`, this.chat.deleteUser);
     this.router.post(`${this.path}/conversation`, this.chat.createConversation);
+    // this.router.get(`${this.path}/getAllConversation`, this.chat.getAllConversations);
+    this.router.get(`${this.path}/getUserConversation/:userId`, this.chat.getUserConversations);
     this.router.post(`${this.path}/sendmsg/:id`, this.chat.sendMessage);
     this.router.post(`${this.path}/addparticipant/:id`, this.chat.addParticipant);
 
-    this.router.get(`${this.path}/getallconversation`, this.chat.getConversations);
+    this.router.get(`${this.path}/getallconversation`, AuthMiddleware, this.chat.getConversations);
     this.router.get(`${this.path}/getallmsg/:id`, this.chat.getallMessages);
+    this.router.get(`${this.path}/getmessage/:cID`, this.chat.getMessages);
 
     this.router.delete(`${this.path}/deleteconversation/:id`, this.chat.deleteConversation);
     this.router.delete(`${this.path}/deletemsg/:id`, this.chat.deleteMessage);
