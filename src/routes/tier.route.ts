7b25a22eb37180bebb6eef2af17c00c60f70bb5c
Matt Evans <matte6264@gmail.com>
2023-12-10 14:32:50 -0600

    naming convension

--- src/routes/tier.route.ts
+++ src/routes/tier.route.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import { TierController } from '@/controllers/tier.controller';
-import { CreateTierDto } from '@/dtos/tier.dto';
+import { TierDto } from '@/dtos/tier.dto';
 import { Routes } from '@interfaces/routes.interface';
 import { ValidationMiddleware } from '@middlewares/validation.middleware';
-
+//import { AuthMiddleware } from '@/middlewares/auth.middleware';
 export class TierRoute implements Routes {
   public path = '/tier';
   public router = Router();
-  public tier = new TierController();
+  public tierController = new TierController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.tier.getTiers);
-    this.router.get(`${this.path}/:id`, this.tier.getTierById);
-    this.router.post(`${this.path}`, ValidationMiddleware(CreateTierDto), this.tier.createTier);
-    this.router.delete(`${this.path}/:id`, this.tier.deleteTier);
-    this.router.put(`${this.path}/:id`, ValidationMiddleware(CreateTierDto, true), this.tier.updateTier);
+    this.router.get(`${this.path}`, this.tierController.getTiers);
+    this.router.get(`${this.path}/:id`, this.tierController.getTierById);
+    this.router.post(`${this.path}`, ValidationMiddleware(TierDto), this.tierController.createTier);
+    this.router.delete(`${this.path}/:id`, this.tierController.deleteTier);
+    this.router.put(`${this.path}/:id`, ValidationMiddleware(TierDto, true), this.tierController.updateTier);
   }
 }
