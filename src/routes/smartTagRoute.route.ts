cb1768960cf045462c22cd2f03589aff8f58b0b4
Matt Evans <matte6264@gmail.com>
2023-12-10 14:28:49 -0600

    added: auth middleware to smarttags

--- src/routes/smartTagRoute.route.ts
+++ src/routes/smartTagRoute.route.ts
@@ -3,21 +3,19 @@ import { SmartTagController } from '@/controllers/smartTagController';
 import { CreateSmartTagDto } from '@/dtos/smartTag.dto';
 import { Routes } from '@interfaces/routes.interface';
 import { ValidationMiddleware } from '@middlewares/validation.middleware';
-
+import { AuthMiddleware } from '@/middlewares/auth.middleware';
 export class SmartTagRoute implements Routes {
   public path = '/smarttag';
   public router = Router();
   public smartTag = new SmartTagController();
-
   constructor() {
     this.initializeRoutes();
   }
-
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.smartTag.getSmartTags);
-    this.router.get(`${this.path}/:id`, this.smartTag.getSmartTagById);
-    this.router.post(`${this.path}`, ValidationMiddleware(CreateSmartTagDto), this.smartTag.createSmartTag);
-    this.router.delete(`${this.path}/:id`, this.smartTag.deleteSmartTag);
-    this.router.put(`${this.path}/:id`, ValidationMiddleware(CreateSmartTagDto, true), this.smartTag.updateSmartTag);
+    this.router.get(`${this.path}`, AuthMiddleware, this.smartTag.getSmartTags);
+    this.router.get(`${this.path}/:id`, AuthMiddleware, this.smartTag.getSmartTagById);
+    this.router.post(`${this.path}`, AuthMiddleware, ValidationMiddleware(CreateSmartTagDto), this.smartTag.createSmartTag);
+    this.router.delete(`${this.path}/:id`, AuthMiddleware, this.smartTag.deleteSmartTag);
+    this.router.put(`${this.path}/:id`, AuthMiddleware, ValidationMiddleware(CreateSmartTagDto, true), this.smartTag.updateSmartTag);
   }
 }
