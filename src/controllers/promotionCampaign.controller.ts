7db144a1304d4bc2ee81cf798e54f31c576d021d
Matt Evans <matte6264@gmail.com>
2023-12-10 16:33:00 -0600

    implemeted: REST API for promotion campaign

---
+++ src/controllers/promotionCampaign.controller.ts
@@ -0,0 +1,65 @@
+import { PromotionCampaign } from '@/interfaces/promotionCampaign.interface';
+import { PromotionCampaignService } from '@/services/promotionCampaign.service';
+import { NextFunction, Request, Response } from 'express';
+import { Container } from 'typedi';
+
+export class PromotionCampaignController {
+  private readonly promotionCampaignService: PromotionCampaignService;
+  constructor() {
+    this.promotionCampaignService = new PromotionCampaignService();
+  }
+  public promotionCampaign = Container.get(PromotionCampaignService);
+
+  public createPromotionCampaign = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const promotionCampaignData: PromotionCampaign = req.body;
+      const createPromotionCampaignData: PromotionCampaign = await this.promotionCampaign.createPromotionCampaign(promotionCampaignData);
+
+      res.status(201).json({ data: createPromotionCampaignData, message: 'PromotionCampaigns created Successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
+  public getPromotionCampaigns = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const getAllPromotionCampaigns: PromotionCampaign[] = await this.promotionCampaign.getPromotionCampaigns();
+      res.status(200).json({ data: getAllPromotionCampaigns, message: 'findAllPromotionCampaigns' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getPromotionCampaignById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const promotionCampaignId: string = req.params.id;
+      const findOnePromotionCampaignData: PromotionCampaign = await this.promotionCampaign.getPromotionCampaignById(promotionCampaignId);
+
+      res.status(200).json({ data: findOnePromotionCampaignData, message: 'findOne' });
+    } catch (error) {
+      next(error);
+    }
+  };
+  public updatePromotionCampaign = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const promotionCampaignId: string = req.params.id;
+      const promotionCampaignData: PromotionCampaign = req.body;
+      const updatePromotionCampaignData: PromotionCampaign = await this.promotionCampaign.updatePromotionCampaign(
+        promotionCampaignId,
+        promotionCampaignData,
+      );
+
+      res.status(200).json({ data: updatePromotionCampaignData, message: 'promotion campaign updated successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
+  public deletePromotionCampaign = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const promotionCampaignId: string = req.params.id;
+      const deletePromotionCampaignData = await this.promotionCampaign.deletePromotionCampaign(promotionCampaignId);
+      res.status(200).json({ data: deletePromotionCampaignData, message: 'promotion campaign deleted' });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
---
+++ src/routes/promotionCampaign.route.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { PromotionCampaignController } from '@/controllers/promotionCampaign.controller';
+import { PromotionCampaignDto } from '@/dtos/promotionCampaign.dto';
+import { Routes } from '@interfaces/routes.interface';
+import { ValidationMiddleware } from '@middlewares/validation.middleware';
+import { AuthMiddleware } from '@/middlewares/auth.middleware';
+export class PromotionCampaignRoute implements Routes {
+  public path = '/promotion';
+  public router = Router();
+  public promotionCampaign = new PromotionCampaignController();
+  constructor() {
+    this.initializeRoutes();
+  }
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, AuthMiddleware, this.promotionCampaign.getPromotionCampaigns);
+    this.router.get(`${this.path}/:id`, AuthMiddleware, this.promotionCampaign.getPromotionCampaignById);
+    this.router.post(`${this.path}`, AuthMiddleware, ValidationMiddleware(PromotionCampaignDto), this.promotionCampaign.createPromotionCampaign);
+    this.router.delete(`${this.path}/:id`, AuthMiddleware, this.promotionCampaign.deletePromotionCampaign);
+    this.router.put(
+      `${this.path}/:id`,
+      AuthMiddleware,
+      ValidationMiddleware(PromotionCampaignDto, true),
+      this.promotionCampaign.updatePromotionCampaign,
+    );
+  }
+}
